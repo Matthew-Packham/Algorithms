64388e5e879eab3ebfb242785fc1700023d32a96
Matthew-Packham <87229493+Matthew-Packham@users.noreply.github.com>
2021-10-14 10:35:09 +0100

    Add files via upload

---
+++ merge_module.py
@@ -0,0 +1,51 @@
+
+def merge(L,R):
+
+    # two half lists which are already sorted - we are going to merge them
+    
+    n = len(L)
+    M = []
+
+    #indices 
+
+    iL = 0
+    iR = 0
+
+    # n is the len of each sub list. we therfore iterate 2n times
+    for i in range(2*n):
+    #FOR L
+        #compare iL and iR elements of L and R
+        if L[iL]<R[iR]:
+            #place the smaller elm in M and increment iL or iR resp
+            M.append(L[iL])
+            iL = iL + 1
+
+
+            #check if we have reached end of list, whose index was incremented 
+            if iL==n:
+                M.extend(R[iR:])
+                break
+
+            # if true, increment remaining preportion
+    #For R
+        else:
+            #place the smaller elm in M and increment iL or iR resp
+            M.append(R[iR])
+            iR = iR + 1
+
+
+            #check if we have reached end of list, whose index was incremented 
+            if iR ==n:
+                M.extend(L[iL:])
+                break
+
+            # if true, increment remaining preportion
+    return M
+
+    
+
+if __name__ == "__main__":
+    L = [2, 6, 24, 56]
+    R = [8, 23, 31, 54]
+    M = merge(L,R)
+    print(M)
---
+++ merge_sort.py
@@ -0,0 +1,17 @@
+import merge_module
+
+def msort(A):
+    nn = len(A)
+    if nn==1:
+        return A
+    else: # split list and call msort in a recursive fashion on left and right lists
+        split = int(nn/2)
+        L = msort(A[:split]) # by calling msort again we keep split the list untill we get len one!
+        R = msort(A[split:])
+        M = merge_module.merge(L, R)
+        return M
+
+if __name__ == "__main__":
+    A = [1,12,34,2,14,24,5,6] # list has to be powers of 2 ie 2, 4, 8, 16, 32, .... so that the spliting works! (can be fixed) 
+    sorter = msort(A)
+    print(sorter)
