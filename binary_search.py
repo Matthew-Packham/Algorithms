141165e8b7b3b04caf033278be48d4444be83cf3
Matthew-Packham <87229493+Matthew-Packham@users.noreply.github.com>
2021-10-14 12:08:21 +0100

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,29 @@
+def binary_search(L,x):
+    """Binary search algorithm to find target x in list L
+    """ 
+    
+    #Set initial start and end indices for full list
+    istart = 0
+    iend = len(L)-1
+    #Contract "active" portion of list
+    while istart<=iend:
+        imid = int(0.5*(istart+iend))
+        if x==L[imid]:
+            return imid
+        elif x < L[imid]:
+            #cut off right half of list
+            iend = imid-1
+        else:
+            #cut off left half of list
+            istart = imid+1
+        #we keep spliting and cutting untill we land on element we are looking for.
+        #we keep tract of element index through imid
+    return -1
+
+
+if __name__ == "__main__":
+    L = [1,4, 6, 13, 35, 66, 79, 89, 90]
+    #set target
+    x = 89
+
+    print(f"index of element {x} is {binary_search(L, x)}")
